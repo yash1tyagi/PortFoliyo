397044340f7832dec306f0d46cc59d2b23fd2bc6
yash1tyagi <yashtyagi65106@gmail.com>
2022-11-30 12:21:20 +0530

    my Website

---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+// import HeroImage from "../assets/heroImage.png";
+// import myimg from "../assets/myImg.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-12">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I have 1 years of experience building and desgining software.
+            Currently, I love to work on web application using technologies like
+            React, Tailwind, Express JS, Node Js and MongoDb .
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          {/* <img
+            // src={HeroImage}
+            src={myimg}
+            alt="my profile"
+            className="rounded-2xl  h-3/5 mx-auto w-2/3 md:w-full"
+            // className="rounded-2xl h-64  w-2/3 md:w-full"
+            // className="h-94 w-64"
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home
